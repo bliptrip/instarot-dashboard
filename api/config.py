5d65930311cdfc862c43e37261da9991135b88db
bliptrip <maule2@wisc.edu>
2026-03-07 18:35:59 -0600

    Changed retention rates for different resolutions of data.

--- api/config.py
+++ api/config.py
@@ -5,10 +5,10 @@ MQTT_TOPIC    = "instarot/+/state"
 DB_PATH       = "/home/maule/applications/instarot-dashboard/data/instarot.db"
 
 RETENTION = {
-    "raw":   60 * 60 * 24,
-    "1min":  60 * 60 * 24 * 7,
-    "10min": 60 * 60 * 24 * 30,
-    "1hr":   60 * 60 * 24 * 365,
+    "raw":   60 * 60 * 24 * 7, #Store for a week - so that user can pull session data for debugging/viewing
+    "1min":  60 * 60 * 24 * 30, #Store 1-min averages for a month
+    "10min": 60 * 60 * 24 * 182, #Store 10-min averages for ~1/2 year
+    "1hr":   60 * 60 * 24 * 365, #Store 1-hour averages for a year
 }
 
 WRITE_BUFFER_INTERVAL = 10
